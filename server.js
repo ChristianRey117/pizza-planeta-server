c82c460f8fcb9485c694ca8d45ad76bf99069d8a
Christian <christianreyeshdz117@gmail.com>
2023-10-11 17:02:28 -0600

    fix(cors): fix cors for client

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
 const express = require("express");
-const  conexion = require('./database');
+const conexion = require("./database");
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
+const cors = require("cors");
 
-app.set('port', process.env.PORT || 5000);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
+app.set("port", process.env.PORT || 5000);
 
 //SERVER RUNNING ----------------------------------
-app.listen(app.get('port'),() => {
-  console.log("Server started on port", app.get('port'))
+app.listen(app.get("port"), () => {
+  console.log("Server started on port", app.get("port"));
 });
-
-
 
 ///ROUTES -------------------------------
 
-app.get('/', (req, res) => {
-  res.send('My api node')
+app.get("/", (req, res) => {
+  res.send("My api node");
 });
 
-app.use('/sucursales', routes)
-
-
-
-
-
-
+app.use("/sucursales", routes);
