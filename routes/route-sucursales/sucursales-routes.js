1664379c7fcf5c4623c6c5ca1d5ad894871e31f2
Elsynt11 <elsynoh11@gmail.com>
2023-11-07 11:31:46 -0600

    get y getID

--- routes/route-sucursales/sucursales-routes.js
+++ routes/route-sucursales/sucursales-routes.js
@@ -20,6 +20,31 @@ const uploadImage = multer({
 
 const controller = require("../../controller/customController");
 
+
+routes.get("/", (req, res) => {
+  conexion.query(
+    "SELECT branch.id_branch, branch.branch_name, branch.branch_direction, branch.work_personnel, branch.image, " + 
+    "GROUP_CONCAT(supplier.id_supplier SEPARATOR ', ') AS ids_suppliers, " +
+    "GROUP_CONCAT(supplier.supplier_name SEPARATOR ', ') AS suppliers " +
+    "FROM branch " +
+    "JOIN supplier_branch ON branch.id_branch = supplier_branch.id_branch " +
+    "JOIN supplier ON supplier_branch.id_supplier = supplier.id_supplier " + 
+    "GROUP BY branch.id_branch ",
+    (err, rows) => {
+      if (err) {
+        //res.send({ err: "Error al conectar con la base de datos" });
+        console.log(err);
+      }
+      res.json(rows);
+    }
+  );
+});
+
+
+/***ANTIGUO GET */
+
+
+/*
 routes.get("/", (req, res) => {
   conexion.query(
     "SELECT branch.id_branch, branch.branch_name, branch.branch_direction, branch.work_personnel, branch.image " +
@@ -32,16 +57,24 @@ routes.get("/", (req, res) => {
     }
   );
 });
+*/
+
 
 routes.get("/:id_branch", (req, res) =>{
   var id = req.params.id_branch;
   conexion.query(
-    "SELECT branch.id_branch, branch.branch_name, branch.branch_direction, branch.work_personnel, branch.image " +
+    "SELECT branch.id_branch, branch.branch_name, branch.branch_direction, branch.work_personnel, branch.image, " + 
+    "GROUP_CONCAT(supplier.id_supplier SEPARATOR ', ') AS ids_suppliers, " +
+    "GROUP_CONCAT(supplier.supplier_name SEPARATOR ', ') AS suppliers " +
     "FROM branch " +
-    "WHERE branch.id_branch = ?", 
+    "JOIN supplier_branch ON branch.id_branch = supplier_branch.id_branch " +
+    "JOIN supplier ON supplier_branch.id_supplier = supplier.id_supplier " + 
+    "WHERE branch.id_branch = ? " +
+    "GROUP BY branch.id_branch ", 
     [id], (err, rows) => {
       if(err) {
-        res.send({err: "No se encontro el registro"});
+        console.log(err);
+        //res.send({err: "No se encontro el registro"});
       }
       res.json(rows);      
     }
